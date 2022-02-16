5dbf74acec2836c99b9c5f4c23d85ca025eeac1f
Sunil Jalandhra <jalandhrasunil99@gmail.com>
2022-02-16 13:48:57 +0530

    done the problem of merge_two_list

---
+++ merge_two_list.rs
@@ -0,0 +1,37 @@
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn merge_two_lists(list1: Option<Box<ListNode>>, list2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        match (list1,list2) {
+            (None,None) => None,
+            (Some(n),None)|(None,Some(n)) => Some(n),
+            (Some(list1),Some(list2)) => {
+                if list1.val >= list2.val {
+                    Some(Box::new(ListNode{
+                        val:list2.val,
+                        next: Solution::merge_two_lists(Some(list1),list2.next)
+                    }))
+                }
+                else {
+                    Some(Box::new(ListNode{
+                        val:list1.val,
+                        next: Solution::merge_two_lists(Some(list2),list1.next)
+                    }))
+                }
+            }
+        }
+    }
+}
